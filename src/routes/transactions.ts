6ac3a6f0d44d18e0f5a5745ca66ba67888f569a4
Alan Reis <alanreisanjo@gmail.com>
2024-11-15 20:04:10 -0300

    AAA

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -7,6 +7,35 @@ export default async function (app: FastifyInstance) {
     return await knex("transactions").select();
   });
 
+  app.get("/:id", async (request, reply) => {
+    const getTransactionIdParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const getTransactionIdParamsSchemaParsed =
+      getTransactionIdParamsSchema.safeParse(request.params);
+
+    if (!getTransactionIdParamsSchemaParsed.success) {
+      return reply.status(400).send({
+        message: `🚫 Bad Request at ${request.url}(${request.method})`,
+        cause: getTransactionIdParamsSchemaParsed.error.issues,
+      });
+    }
+
+    const { id } = request.params as { id: string };
+
+    const transaction = await knex("transactions").where("id", id).first().select("title", "type", "amount");
+
+    if (!transaction) {
+      return reply.status(404).send({
+        message: `⛔ Not Found at ${request.url}(${request.method})`,
+        cause: `Transaction not found ${id}`,
+      });
+    }
+
+    return reply.status(200).send(transaction);
+  });
+
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -28,12 +57,18 @@ export default async function (app: FastifyInstance) {
       });
     }
 
-    await knex("transactions").insert({
-      title,
-      amount: type === "credit" ? amount : -amount,
-      type,
-    });
+    const transactionId = await knex("transactions")
+      .insert({
+        title,
+        amount: type === "credit" ? amount : -amount,
+        type,
+      })
+      .returning("id");
+    
+      console.log(transactionId)
 
-    return reply.status(201).send("✅ Transaction was been sucessful");
+    return reply
+      .status(201)
+      .send(`✅ Transaction was been sucessful: ${transactionId[0].id}`);
   });
 }
