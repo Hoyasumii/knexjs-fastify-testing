e2b0d8eb32c75f89ab62d927ffa6d8add27de5ea
Alan Reis <alanreisanjo@gmail.com>
2024-11-18 19:10:04 -0300

    Criado múltiplos .env

--- src/database/config.ts
+++ src/database/config.ts
@@ -3,7 +3,9 @@ import { Knex } from "knex";
 export const config: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: `${process.env.DB_PATH}/content.db`,
+    filename: `${process.env.DB_PATH}/${
+      process.env.NODE_ENV === "test" ? "content.test.db" : "content.db"
+    }`,
   },
   useNullAsDefault: true,
   migrations: {
