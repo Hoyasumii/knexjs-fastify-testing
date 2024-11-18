ae1b535c2765dd80292fcbf5f038a368cfe36e34
Alan Reis <alanreisanjo@gmail.com>
2024-11-18 17:16:23 -0300

    Adicionado Vitest + Cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import fastify from "fastify";
-import { faker } from "@faker-js/faker";
 import cookie from "@fastify/cookie";
+import cors from "@fastify/cors";
+import { faker } from "@faker-js/faker";
 import knex from "./database";
 import logger from "./logger";
 import { transactions } from "./routes";
 
 const app = fastify({ logger });
+
+app.register(cors, {
+  credentials: true,
+});
 
 app.register(cookie, {
   secret: process.env.COOKIES_SECRET,
